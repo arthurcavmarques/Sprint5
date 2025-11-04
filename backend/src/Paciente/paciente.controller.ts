68e5c1b34d745529760011e2e88662882a1f595e
vitorgabriel-coder <vitor.collatto@edu.pucrs.br>
2025-11-04 14:35:33 -0300

    Criação dos dtos de update e o CRUD de paciente

--- backend/src/Paciente/paciente.controller.ts
+++ backend/src/Paciente/paciente.controller.ts
@@ -1,12 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, Post, Delete, Patch, Body, UseGuards } from '@nestjs/common';
 import { PacienteService } from './paciente.service';
+import { Paciente } from './paciente';
+import { UpdatePatientDto } from './atualizar.paciente';
 
-@Controller()
+@Controller('paciente')
 export class PacienteController {
   constructor(private readonly pacienteService: PacienteService) {}
 
   @Get()
-  getHello(): string {
-    return this.pacienteService.getHello();
+  findAll() {
+    return this.pacienteService.findAll();
   }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+
+    const paciente = await this.pacienteService.findOne(id);
+
+    if(!paciente){
+      throw new NotFoundException(`Paciente com ID ${id} não encontrado.`);
+    }
+
+    return paciente;
+
+  }
+
+  @Post()
+  create(@Body() createPacienteDto: Paciente) {
+    return this.pacienteService.create(createPacienteDto);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePacienteDto: UpdatePatientDto) {
+    return this.pacienteService.update(id, updatePacienteDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.pacienteService.remove(id);
+  }
+
 }
--- backend/src/Paciente/paciente.service.ts
+++ backend/src/Paciente/paciente.service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../Prisma/prisma.service';
+import { Paciente } from './paciente';
+import { UpdatePatientDto } from './atualizar.paciente';
 
 @Injectable()
 export class PacienteService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Paciente) {
+    return this.prisma.paciente.create({ data });
   }
+
+  async findAll() {
+    return this.prisma.paciente.findMany();
+  }
+
+  async findOne(id: string) {
+    return this.prisma.paciente.findUnique({ where: { id } });
+  }
+
+  async update(id: string, data: UpdatePatientDto) {
+    return this.prisma.paciente.update({ where: { id }, data });
+  }
+
+  async remove(id: string) {
+    return this.prisma.paciente.delete({ where: { id } });
+  }
+
 }
