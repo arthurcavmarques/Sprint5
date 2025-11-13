62500bf4949ac3d631eafc7819766f0619111aad
vitorgabriel-coder <vitor.collatto@edu.pucrs.br>
2025-11-13 15:20:48 -0300

    corrigindo evoulacao

--- backend/src/Evolucao/evolucao.controller.ts
+++ backend/src/Evolucao/evolucao.controller.ts
@@ -5,30 +5,30 @@ import { UpdateEvolucaoDTO } from './atualizar.evolucao';
 
 @Controller()
 export class EvolucaoController {
-  constructor(private readonly evoulacaoService: EvolucaoService) {}
+  constructor(private readonly evolucaoService: EvolucaoService) {}
 
   @Post('evolucao')
   async create(@Body() req: EvolucaoDTO) {
-    return await this.evoulacaoService.create(req)
+    return await this.evolucaoService.create(req)
   }
 
   @Get('evolucao')
   async listAll() {
-    return await this.evoulacaoService.listAll()
+    return await this.evolucaoService.listAll()
   }
 
   @Get('evolucao/:id')
-  async searchById(@Param('id', ParseIntPipe),  id : number) {
-    return await this.evoulacaoService.searchById(id)
+  async searchById(@Param('id', ParseIntPipe)  id : number) {
+    return await this.evolucaoService.searchById(id)
   }
 
   @Patch('evolucao/:id')
   async updateEvolucao(@Body() req : UpdateEvolucaoDTO, @Param('id', ParseIntPipe) id : number) {
-      return await this.evoulacaoService.updateEvolucao(req, id)
+      return await this.evolucaoService.updateEvolucao(req, id)
   }
 
   @Delete('evolucao/:id')
   async deleteEvolucao(@Param('id', ParseIntPipe) id : number) {
-    return this.evoulacaoService.deleteEvolucao(id)
+    return this.evolucaoService.deleteEvolucao(id)
   }
 }
