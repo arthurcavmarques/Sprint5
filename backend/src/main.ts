44af3a3c1f22ef91bb77dbefa5c429db0d81b6e3
arthurcmarques <cavalheiro.arthur2006@gmail.com>
2025-11-24 19:25:37 -0300

    Comecando a integracao

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,14 @@ import { AppModule } from './App/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // CORREÇÃO: CORS deve vir ANTES de ouvir a porta
   app.enableCors({
-    origin: 'http://localhost:5173', // Permite requisições do seu frontend
+    origin: 'http://localhost:5173',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
+
+  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
